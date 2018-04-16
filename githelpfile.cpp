4c9a44b07ff8ccaeaa470afa7355cd044e44f33c
Susana <susana56@g.ucla.edu>
2018-04-16 14:03:46 -0700

    Newest updates

--- githelpfile.cpp
+++ githelpfile.cpp
@@ -1,3 +1,5 @@
 This a the fourth file, creating along git video from lynda.
 
-Creating an edit , this is that edit. 
+Creating an edit , this is that edit.
+
+I am going to write this sentence as a change to use git diff and notice the changes as in the reopository vs working. 
