31a98cf6de9340529491e2d7862424814650035e
Susana <susana56@g.ucla.edu>
2018-04-17 10:12:03 -0700

    Minor text edits

--- githelpfile.cpp
+++ githelpfile.cpp
@@ -3,3 +3,5 @@ This a the fourth file, creating along git video from lynda.
 Creating an edit , this is that edit.
 
 I am going to write this sentence as a change to use git diff and notice the changes as in the reopository vs working. 
+
+This time it is for the "viewing only stanged changes video"
